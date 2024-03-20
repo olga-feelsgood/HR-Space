8a6234c7b7aac1bd34402b0f560f3b86c82de0c9
Leyla <lejla.eyvazova@gmail.com>
2024-03-20 17:26:54 +0700

    feat: add stephome w/t validation

--- frontend/src/components/StepHome/StepHome.jsx
+++ frontend/src/components/StepHome/StepHome.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import './StepHome.css'
+import { useForm } from '../../hooks/useForm';
+import Button from '../Button/Button';
 
 function StepHome() {
+  const { values, handleChange, errorMessage } = useForm({ vacancyName: '' });
+
   return (
-    <div>StepHome</div>
-  )
+    <form className="stephome">
+      <h1 className="stephome__title">Название вакансии</h1>
+      <input
+        className="stephome__input"
+        type="text"
+        value={values.vacancyName}
+        onChange={handleChange}
+        name="vacancyName"
+        placeholder="Введите название вакансии"
+      />
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <Button />
+    </form>
+  );
 }
 
-export default StepHome
+export default StepHome;
