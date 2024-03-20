661e5542a324e727a665d2699218ff395296cb67
Leyla <lejla.eyvazova@gmail.com>
2024-03-20 23:52:56 +0700

    feat: add inputs in StepWorkingHours

--- frontend/src/components/StepSalary/StepSalary.jsx
+++ frontend/src/components/StepSalary/StepSalary.jsx
@@ -7,14 +7,38 @@ import { useNavigate } from 'react-router-dom';
 function StepSalary() {
   //пока что костыль, когда сделаем логику, будем брать из useFrom
   const [stepIsValid, setStepIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
   let navigate = useNavigate();
   const onRedirect = () => navigate('/jobdescription/workinghours');
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <>
       <div className='office-remote__container'>
-        <div> StepSalary</div>
+        <h1 className="salary__title">Заработная плата до вычета НДФЛ</h1>
+        <div className="salary__inputs">
+          <input
+            className="salary__input"
+            type="text"
+            name="from"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="От"
+          />
+          <input
+            className="salary__input"
+            type="text"
+            name="to"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="До"
+          />
+          <p className="salary__text">₽</p>
+        </div>
         <div className='office-remote__button'>
           <Button
             onClick={onRedirect}
--- frontend/src/components/StepWorkingHours/StepWorkingHours.jsx
+++ frontend/src/components/StepWorkingHours/StepWorkingHours.jsx
@@ -7,15 +7,39 @@ import { useNavigate } from 'react-router-dom';
 function StepWorkingHours() {
   //пока что костыль, когда сделаем логику, будем брать из useFrom
   const [stepIsValid, setStepIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
   let navigate = useNavigate();
   const onRedirect = () => navigate('/jobdescription/employmenttype');
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+
   return (
     <>
       <div className='office-remote__container'>
-        <div> StepWorkingHours</div>
+        <div className="working-hours__inputs">
+          <input
+            className="working-hours__input"
+            type="text"
+            name="the begin of the workday"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Начало рабочего дня"
+          />
+          <input
+            className="working-hours__input"
+            type="text"
+            name="the end of the worday"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Конец рабочего дня"
+          />
+        </div>
         <div className='office-remote__button'>
+
           <Button
             onClick={onRedirect}
             stepIsValid={stepIsValid}
