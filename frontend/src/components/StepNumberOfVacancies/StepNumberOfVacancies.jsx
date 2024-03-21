46b72dd0c76b224238b9a65420b5084b14b72cfd
Leyla <lejla.eyvazova@gmail.com>
2024-03-21 20:30:51 +0700

    add css in StepNumberOfVacancies

--- frontend/src/components/StepNumberOfVacancies/StepNumberOfVacancies.jsx
+++ frontend/src/components/StepNumberOfVacancies/StepNumberOfVacancies.jsx
@@ -7,14 +7,31 @@ import { useNavigate } from 'react-router-dom';
 function StepNumberOfVacancies() {
   //пока что костыль, когда сделаем логику, будем брать из useFrom
   const [stepIsValid, setStepIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
   let navigate = useNavigate();
   const onRedirect = () => navigate('/workingconditions/typesofreward');
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <>
       <div className='number-vacancies__container'>
-        <div> StepNumberOfVacancies</div>
+        <div>
+
+          <input
+            className="number-vacancies__input"
+            type="text"
+            name="cityName"
+            list="cities"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Введите число сотрудников для поиска"
+          />
+
+        </div>
         <div className='number-vacancies__button'>
           <Button
             onClick={onRedirect}
