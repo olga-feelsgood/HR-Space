a3b32c1139335ff4b123c77718bf95cd684b2739
Leyla <lejla.eyvazova@gmail.com>
2024-03-22 22:29:05 +0700

    fix calendar in StepCandidateFirstWorkingDay

--- frontend/src/components/StepCandidateFirstWorkingDay/StepCandidateFirstWorkingDay.jsx
+++ frontend/src/components/StepCandidateFirstWorkingDay/StepCandidateFirstWorkingDay.jsx
@@ -3,31 +3,36 @@ import Button from '../Button/Button.jsx'
 import CurrentApplicationBox from '../CurrentAplicationBox/CurrentApplicationBox';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 
 
 function StepCandidateFirstWorkingDay() {
   //пока что костыль, когда сделаем логику, будем брать из useFrom
   const [stepIsValid, setStepIsValid] = useState(true);
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [dateValue, setDateValue] = useState('');
 
 
   let navigate = useNavigate();
   const onRedirect = () => navigate('/workingconditions/interview');
 
+  const handleInputChange = event => {
+    dateValue(event.target.value);
+  };
+
   return (
     <>
       <div className='candidate-firstday__container'>
         <div>
-          <DatePicker
-            showIcon
-            toggleCalendarOnIconClick
-            selected={selectedDate}
-            placeholderText="Выберите дату"
-            className="candidate-firstday__picker"
-            onChange={(date) => setSelectedDate(date)}
+          <input
+            className='candidate-firstday__input'
+            type='text'
+            name='date'
+            value={dateValue}
+            onChange={handleInputChange}
+            placeholder='ДД.ММ.ГГГГ'
           />
+          <span>По статистике площадки средний срок закрытия вакансии — 1 месяц.</span>
         </div>
         <CurrentApplicationBox />
         <div className='candidate-firstday__button'>
